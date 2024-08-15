11846c632af6bcf82ae835f774c0d05a0a1add5a
Loan CLERIS <loan.cleris@gmail.com>
2024-08-15 22:05:46 +0200

    feat(router): init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import Router from './router/router'
+import './style/App.css'
 
 function App() {
-  return <div className="flex w-full bg-slate-100 h-2">Casa dana</div>
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
---
+++ src/router/router.tsx
@@ -0,0 +1,49 @@
+import { Fragment, PropsWithChildren, Suspense, lazy } from 'react'
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom'
+
+const Home = lazy(() => import('../pages/Home'))
+
+interface ProtectedRouteProps extends PropsWithChildren {
+  redirect: string
+  condition: boolean
+}
+
+/**
+ * This is a TypeScript React function that renders children if a condition is met, otherwise it
+ * redirects to a specified route.
+ * @param {ProtectedRouteProps}  - - `redirect`: the path to redirect to if the `condition` is false
+ * @returns The `ProtectedRoute` component is being returned. If the `condition` prop is true, it will
+ * render the `children` prop wrapped in a `React.Fragment`. If the `condition` prop is false, it will
+ * render a `Navigate` component with the `to` prop set to the `redirect` prop.
+ */
+const ProtectedRoute = ({
+  redirect,
+  condition,
+  children
+}: ProtectedRouteProps) => {
+  if (condition)
+    return (
+      <Fragment>
+        <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+      </Fragment>
+    )
+  else return <Navigate to={redirect} />
+}
+
+function Router() {
+  return (
+    <Routes>
+      <Route
+        element={
+          <ProtectedRoute condition={true} redirect="/">
+            <Outlet />
+          </ProtectedRoute>
+        }
+      >
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default Router
