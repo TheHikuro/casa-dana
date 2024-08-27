bf039e40db9384757fecfb2ede0c41e148724ee7
Loan CLERIS <loan.cleris@gmail.com>
2024-08-27 23:41:44 +0200

    fix(i18n): hot reload on chang lang

--- src/i18n/hook/useLang.tsx
+++ src/i18n/hook/useLang.tsx
@@ -3,13 +3,13 @@ import { ACCEPTED_LANGUAGES } from '../../utils/enum'
 import { useCallback } from 'react'
 
 export function useLang() {
+  const currentLanguage = i18next.language
+
   const handleChangeLanguage = useCallback((lang: ACCEPTED_LANGUAGES) => {
     i18next.changeLanguage(lang)
-    i18next.reloadResources(lang)
     localStorage.setItem('selectedLanguage', lang)
+    window.location.reload()
   }, [])
-
-  const currentLanguage = i18next.language
 
   return { handleChangeLanguage, currentLanguage }
 }
