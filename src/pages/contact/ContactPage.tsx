4058d7c8cac9ba67dc84ff48fb2b3fb846a3ee44
Loan CLERIS <loan.cleris@gmail.com>
2024-12-28 01:04:20 +0100

    feat(calendar): add style

--- src/pages/contact/ContactPage.tsx
+++ src/pages/contact/ContactPage.tsx
@@ -1,12 +1,14 @@
+import { useNavigate } from 'react-router-dom'
 import Fresque from '../../assets/fresques/fresque.png'
 import Logo from '../../assets/logo/logo_header.png'
 import { Footer } from '../../components'
 import { ContactCalendar, ContactForm } from './_components'
 
 export default function ContactPage() {
+  const navigate = useNavigate()
   return (
     <div className="flex flex-col h-dvh">
-      <div className="w-full h-56 relative flex justify-center">
+      <div className="w-full h-56 relative flex justify-center z-0">
         <img
           src={Fresque}
           alt={'casa_dana_fresque'}
@@ -15,14 +17,15 @@ export default function ContactPage() {
         <img
           src={Logo}
           alt="casa_dana_logo"
-          className="w-36 h-36 absolute top-14"
+          className="w-36 h-36 absolute top-14 cursor-pointer"
+          onClick={() => navigate('/')}
         />
       </div>
       <div className="h-full flex">
         <div className="w-1/2 flex justify-center items-center">
           <ContactForm />
         </div>
-        <div className="w-1/2 flex justify-center items-center">
+        <div className="w-1/2 flex justify-center items-center z-10">
           <ContactCalendar />
         </div>
       </div>
--- src/pages/contact/_components/ContactCalendar.tsx
+++ src/pages/contact/_components/ContactCalendar.tsx
@@ -1,7 +1,7 @@
 import Calendar from 'react-calendar'
 import { useState } from 'react'
-import 'react-calendar/dist/Calendar.css'
 import { useLang } from '../../../i18n/hook/useLang.tsx'
+import './calendar.style.css'
 
 type ValuePiece = Date | null
 
@@ -18,6 +18,14 @@ export function ContactCalendar() {
       locale={currentLanguage}
       selectRange={!!value}
       minDate={new Date()}
+      prev2Label={null}
+      next2Label={null}
+      className="p-4 bg-gray-50 border border-gray-200 rounded-lg shadow-lg w-2/3"
+      tileClassName={({ date }) =>
+        date.toDateString() === new Date().toDateString()
+          ? 'bg-blue-500 text-white rounded-full my-0.5 mx-2 drop-shadow-md'
+          : 'hover:bg-gray-100 rounded-lg my-0.5 mx-2 drop-shadow-md'
+      }
     />
   )
 }
